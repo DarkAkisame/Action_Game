1669525f91a6b5e9e75d45d77d8a62c71dfb89fd
DarkAkisame <38438021+DarkAkisame@users.noreply.github.com>
2018-11-05 08:50:38 -0500

    TheFirstComit

---
+++ Source/ActionGame/CharacterBase.cpp
@@ -0,0 +1,136 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CharacterBase.h"
+#include "AttributeSetBase.h"
+#include "GameFramework/PlayerController.h"
+#include "AIController.h"
+#include "BrainComponent.h"
+
+// Sets default values
+ACharacterBase::ACharacterBase()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AbilitySystemComp = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComp");
+	AttributeSetBaseComp = CreateDefaultSubobject<UAttributeSetBase>("AttributeSetBaseComp"); \
+	isDead = false;
+	TeamID = 252;
+}
+
+// Called when the game starts or when spawned
+void ACharacterBase::BeginPlay()
+{
+	
+	Super::BeginPlay();
+	///health
+	AttributeSetBaseComp->OnHealthChange.AddDynamic(this, &ACharacterBase::OnHealthChanged);
+	///Mana
+	AttributeSetBaseComp->OnManaChange.AddDynamic(this, &ACharacterBase::OnManaChanged);
+	///Strength
+	AttributeSetBaseComp->OnStrengthChange.AddDynamic(this, &ACharacterBase::OnStrengthChanged);
+
+	AutoDeterminTeamIDbyControllerType();
+	AddGameplayTag(FullHealthTag);
+}
+
+// Called every frame
+void ACharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+UAbilitySystemComponent * ACharacterBase::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComp;
+}
+
+void ACharacterBase::AquireAbility(TSubclassOf<UGameplayAbility> AbilityToAquire)
+{
+	if (AbilitySystemComp)
+	{
+		if (HasAuthority() && AbilityToAquire)
+		{
+			FGameplayAbilitySpecDef SpecDef = FGameplayAbilitySpecDef();
+			SpecDef.Ability = AbilityToAquire;
+			FGameplayAbilitySpec AbilitySpec = FGameplayAbilitySpec(SpecDef, 1);
+			AbilitySystemComp->GiveAbility(AbilitySpec);
+		}
+		AbilitySystemComp->InitAbilityActorInfo(this, this);
+	}
+}
+// health
+void ACharacterBase::OnHealthChanged(float Health, float MaxHealth)
+{
+	if (Health<= 0.0f && !isDead)
+	{
+		isDead = true;
+		Dead();
+		BP_Die();
+	}
+	BP_OnHealthChanged(Health, MaxHealth);
+}
+
+//Mana
+void ACharacterBase::OnManaChanged(float Mana, float MaxMana)
+{
+	BP_OnManaChanged(Mana, MaxMana);
+}
+
+//Strength
+void ACharacterBase::OnStrengthChanged(float Strength, float MaxStrength)
+{
+	BP_OnStrengthChanged(Strength, MaxStrength);
+}
+
+bool ACharacterBase::isOtherHostile(ACharacterBase* Other)
+{
+	return TeamID != Other->getTeamID();
+}
+
+uint8 ACharacterBase::getTeamID() const
+{
+	return TeamID;
+}
+
+void ACharacterBase::AddGameplayTag(FGameplayTag& TagToAdd)
+{
+	GetAbilitySystemComponent()->AddLooseGameplayTag(TagToAdd);
+	GetAbilitySystemComponent()->SetTagMapCount(TagToAdd, 1);
+}
+
+void ACharacterBase::RemoveGameplayTag(FGameplayTag& TagToRemove)
+{
+	GetAbilitySystemComponent()->RemoveLooseGameplayTag(TagToRemove);
+}
+
+void ACharacterBase::AutoDeterminTeamIDbyControllerType()
+{
+	if (GetController() && GetController()->IsPlayerController())
+	{
+		TeamID = 0;
+	}
+}
+
+void ACharacterBase::Dead()
+{
+	APlayerController* PC = Cast<APlayerController>(GetController());
+	if (PC)
+	{
+		PC->DisableInput(PC);
+	}
+	AAIController* AIC = Cast<AAIController>(GetController());
+	if (AIC)
+	{
+		AIC->GetBrainComponent()->StopLogic("Dead");
+	}
+}
+
---
+++ Source/ActionGame/CharacterBase.h
@@ -0,0 +1,82 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystemComponent.h"
+#include "CharacterBase.generated.h"
+
+class UAttributeSetBase;
+
+UCLASS()
+class ACTIONGAME_API ACharacterBase : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACharacterBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "CharacterBase")
+		UAbilitySystemComponent* AbilitySystemComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "CharacterBase")
+		UAttributeSetBase* AttributeSetBaseComp;
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const;
+
+	UFUNCTION(BlueprintCallable, Category = "CharacterBase")
+		void AquireAbility(TSubclassOf<UGameplayAbility> AbilityToAquire);
+	///health
+	UFUNCTION()
+		void OnHealthChanged(float Health, float MaxHealth);
+	UFUNCTION(BlueprintImplementableEvent, Category = "CharacterBase", meta = (DisplayName = "OnHealthChanged"))
+		void BP_OnHealthChanged(float Health, float MaxHealth);
+	///Mana
+	UFUNCTION()
+		void OnManaChanged(float Mana, float MaxMana);
+	UFUNCTION(BlueprintImplementableEvent, Category = "CharacterBase", meta = (DisplayName = "OnManaChanged"))
+		void BP_OnManaChanged(float Mana, float MaxMana);
+	///Strength
+	UFUNCTION()
+		void OnStrengthChanged(float Strength, float MaxStrength);
+	UFUNCTION(BlueprintImplementableEvent, Category = "CharacterBase", meta = (DisplayName = "OnStrengthChanged"))
+		void BP_OnStrengthChanged(float Strength, float MaxStrengtha);
+
+
+	///death
+	UFUNCTION(BlueprintImplementableEvent, Category = "CharacterBase", meta = (DisplayName = "Die"))
+		void BP_Die();
+	UFUNCTION(BlueprintCallable, Category = "CharacterBase")
+		bool isOtherHostile(ACharacterBase* Other);
+	uint8 getTeamID()const;
+
+
+	//Tag's
+	UFUNCTION(BlueprintCallable, Category = "CharacterBase")
+		void AddGameplayTag(FGameplayTag& TagToAdd);
+
+	UFUNCTION(BlueprintCallable, Category = "CharacterBase")
+		void RemoveGameplayTag(FGameplayTag& TagToRemove);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CharacterBase")
+		FGameplayTag FullHealthTag;
+
+protected:
+	bool isDead;
+	uint8 TeamID;
+
+	void AutoDeterminTeamIDbyControllerType();
+	void Dead();
+};
