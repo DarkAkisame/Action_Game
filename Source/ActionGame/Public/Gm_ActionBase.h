34ee3174bfbb1d0e6656cddb0187b1cbd2d1d0bd
DarkAkisame <38438021+DarkAkisame@users.noreply.github.com>
2018-11-21 16:26:58 -0500

    ReSetting up Spawning

--- Source/ActionGame/Public/Gm_ActionBase.h
+++ Source/ActionGame/Public/Gm_ActionBase.h
@@ -14,46 +14,5 @@ class ACTIONGAME_API AGm_ActionBase : public AGameModeBase
 {
 	GENERATED_BODY()
 
-protected:
 
-
-	FTimerHandle TimerHandle_BotSpawner;
-
-	FTimerHandle TimerHandle_NextWaveStart;
-
-	// Bots to spawn in current wave
-	int32 NumOfBotsToSpawn;
-
-	int32 WaveCount;
-
-	UPROPERTY(EditDefaultsOnly, Category = "GameMode")
-	float TimeBetweenWave;
-
-	
-protected:
-
-	UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
-		void SpawnNewBot();
-	
-	void SpawnBotTimerElapsed();
-
-
-	// start spawning AI
-	void StartWave();
-
-	// End spawning AI
-	void EndWave();
-
-
-	// Set timer for  next start wave
-	void PrepareForNextWave();
-
-	// checks wave state
-	void CheckWaveSate();
-
-public:
-
-	AGm_ActionBase();
-
-	virtual void StartPlay() override;
 };
